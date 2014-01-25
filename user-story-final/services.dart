35e71079311e513719ec1627f005673061b7f55c
stefrem <stefremillieux@gmail.com>
2014-01-25 17:36:34 +0100

    Add path to common directory into user-story-final
    Add path to common directory into user-story-final.
    For img and json resources.

--- user-story-final/services.dart
+++ user-story-final/services.dart
@@ -8,13 +8,13 @@ import 'models.dart';
 
 final MovieService moviesService = new InMemoryMoviesService();
 
-typedef Future<List<Movie>> MoviesRetriever(); 
+typedef Future<List<Movie>> MoviesRetriever();
 
 /**
  * Movie service definition
  */
 abstract class MovieService {
-  
+
   Future<Iterable<Movie>> getAllMovies();
   Future<Iterable<Movie>> getFavorites();
   Future<Iterable<Movie>> getMovies(String tag);
@@ -26,26 +26,26 @@ abstract class MovieService {
  * InMemory movie service implementation (for tests purpose - it does not rely on backend)
  */
 class InMemoryMoviesService extends MovieService {
-  
+
   Map<int, Movie> _allMovies = new Map();
-  
+
   /// Retrieve all movies
   Future<Iterable<Movie>> getAllMovies() {
-    if (_allMovies.isEmpty) return _getMovies('json/all.json')..then((List<Movie> m) => _allMovies = new Map.fromIterable(m, key: (Movie m) => m.id));
+    if (_allMovies.isEmpty) return _getMovies('../common/json/all.json')..then((List<Movie> m) => _allMovies = new Map.fromIterable(m, key: (Movie m) => m.id));
     else return new Future(() => _allMovies.values);
   }
   /// Retrieve favorites
   Future<Iterable<Movie>> getFavorites() => new Future(() => _allMovies.values.where((Movie m) => m.favorite).toList());
-  
+
   /// Retrieves movies which have the specified tag
   Future<Iterable<Movie>> getMovies(String tag) => new Future(() => _allMovies.values.where((Movie m) => m.tag == tag).toList());
-  
+
   /// Retrieve a specific movie from its ID
-  Future<MovieDetail> getMovieDetail(int id) => _getMovieDetail('json/movies/${id}.json');
-  
+  Future<MovieDetail> getMovieDetail(int id) => _getMovieDetail('../common/json/movies/${id}.json');
+
   /// Save a movie
   void save(Movie m) { }
-  
+
   // Internal method to retrieve a movie's detail from a json url (also works without server - only dartium)
   Future<MovieDetail> _getMovieDetail(String jsonUrl) {
     Completer completer = new Completer();
@@ -54,7 +54,7 @@ class InMemoryMoviesService extends MovieService {
     });
     return completer.future;
   }
-  
+
   // Internal method to retrieve movies from a json url (also works without server - only dartium)
   Future<List<Movie>> _getMovies(String jsonUrl) {
     Completer completer = new Completer();
