d601fe39f35d53f894d55a7ff8c1cbdabfee590c
stefrem <stefremillieux@gmail.com>
2014-01-25 17:36:34 +0100

    Add path to common directory into user-story-final
    Add path to common directory into user-story-final.
    For img and json resources.

--- user-story-final/models.dart
+++ user-story-final/models.dart
@@ -10,11 +10,11 @@ import 'services.dart';
 
 /// A storage to save movie's local datas
 class MovieStorage {
-  
+
   bool favorite;
   String comment;
   int _movieId;
-  
+
   MovieStorage.fromLocalStorage(this._movieId) {
     try {
       String data = window.localStorage["${_movieId}"];
@@ -27,21 +27,21 @@ class MovieStorage {
       comment = "";
     }
   }
-  
+
   /// Save current values to the local storage
   save() => window.localStorage["${_movieId}"] = '{ "fav" : ${favorite}, "comment" : "${comment}" }';
 }
 
 /// A movie with no detail
 class Movie extends Observable {
-  
+
   // Available comparators
   static final Map _comparators = {
     "title": (Movie a, Movie b) => a.title.compareTo(b.title),
     "vote": (Movie a, Movie b) => a.voteAverage.compareTo(b.voteAverage) * -1,
     "favorite": (Movie a, Movie b) => a.favorite && !b.favorite ? -1 : b.favorite && !a.favorite ? 1 : 0,
   };
-  
+
   @observable int id;
   @observable String tag;
   @observable String title;
@@ -51,29 +51,29 @@ class Movie extends Observable {
   @observable int voteCount;
   @observable bool favorite ;
   @observable String comment;
-  
+
   MovieStorage _storage;
-  
+
   Movie(this.title, this.posterPath);
-  
+
   Movie.sample() {
     id = 1;
     title = "Dart Flight School";
-    posterPath = "img/dart-flight-school.jpg";
+    posterPath = "../common/img/dart-flight-school.jpg";
     releasedDate="2014/02/19";
     voteAverage = 10;
     voteCount = 80;
   }
-  
+
   Movie.fromMap(Map<String, Object> map) {
     id = map['id'];
     tag = map['tag'];
     title = map['title'] != null ? map['title'] : map['original_name'];
-    posterPath = map['poster_path'] != null ? 'json/images/posters${map['poster_path']}' : 'img/no-poster-w130.jpg';
+    posterPath = map['poster_path'] != null ? '../common/json/images/posters${map['poster_path']}' : '../common/img/no-poster-w130.jpg';
     releasedDate = map['release_date'];
     voteAverage = map['vote_average'] != null ? (map['vote_average'] as num).toInt() : 0;
     voteCount = map['vote_count'];
-    
+
     // Load favorite and comment attributes from local storage
     _storage = new MovieStorage.fromLocalStorage(id);
     favorite = _storage.favorite;
@@ -109,10 +109,10 @@ class MovieDetail extends Movie with Observable {
   @observable String trailer;
   @observable String country;
 
-  
+
   /// String.isNotEmpty is removed from tree shaking, so used a getter instead
   @reflectable bool get hasTrailer => trailer != null ? trailer.isNotEmpty : false;
-  
+
   MovieDetail.fromMap(Map<String, Object> map) : super.fromMap(map) {
     genre = map['genres'] != null && (map['genres'] as List).isNotEmpty ? (map['genres'] as List)[0]['name'] : '';
     tagLine = map['tagline']!= null && (map['tagline'] as String).isNotEmpty ? "\"${map['tagline']}\"" : "";
@@ -126,12 +126,12 @@ class MovieDetail extends Movie with Observable {
 
 /// A menu
 class Menu extends Object with Observable {
-  
+
   @observable int id;
   @observable String name;
   @observable bool selected = false;
   MoviesRetriever retriever;
-  
+
   Menu(this.id, this.name, this.retriever, [this.selected = false]);
   Menu.fromMap(Map<String, Object> map) {
     id = map['id'];
